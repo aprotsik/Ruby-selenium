47569310fbc202de9311a0bbf3d91edb54c1d85b
root <root@epbyminw1764t1.epam.com>
2014-12-08 13:34:11 +0300

    Http timeout corrected

--- selenium_uk.rb
+++ selenium_uk.rb
@@ -1,78 +1,162 @@
 #!/usr/bin/env ruby
 
 require 'selenium-webdriver'
+require 'headless'
+require 'fileutils'
+require 'net/http'
 
-driver = Selenium::WebDriver.for :firefox
-site_url = "http://www.thomascook.com"
+headless = Headless.new
+headless.start
+client = Selenium::WebDriver::Remote::Http::Default.new
+client.timeout = 300
+driver = Selenium::WebDriver.for(:firefox, :http_client => client) 
 retval = 0
+screenfile = "#{Time.now.strftime("%d.%m.%Y__%H'%M'%S")}.png"
+
+FileUtils.mkdir_p 'mail'
+FileUtils.rm_rf(Dir.glob('mail/*'))
 
 driver.manage.window.maximize
+driver.manage.timeouts.page_load = 300
 
-wait =  Selenium::WebDriver::Wait.new(:timeout => 30)
+
 
 # Access thomascook.com
-begin 
-  driver.manage.timeouts.implicit_wait = 30
-  driver.navigate.to site_url
-rescue
-  driver.save_screenshot("./#{Time.now.strftime("Failure__%d.%m.%Y__%H:%M:%S")}.png")
-  puts "Error loading #{login_url}"	
+begin
+  driver.navigate.to "http://www.thomascook.com"
+  
+rescue => exception
+  puts exception.backtrace
+  puts "Died loading http://www.thomascook.com"
+  driver.save_screenshot("#{screenfile}")
+  FileUtils.cp("#{screenfile}", "mail")
   driver.quit
-  exit 5
+  headless.destroy
+  exit 5  
 end
 
 # Click search button
 begin
-  wait.until { driver.find_element(:xpath, "//button[@class='btn btn-primary searchPanel-commandBtn searchPanel-searchBtn js-btn-search']").displayed? }
+  url = driver.current_url
+  driver.manage.timeouts.implicit_wait = 300 
   Searchbutton = driver.find_element(:xpath, "//button[@class='btn btn-primary searchPanel-commandBtn searchPanel-searchBtn js-btn-search']")
   Searchbutton.click
-rescue
-  driver.save_screenshot("./#{Time.now.strftime("Failure__%d.%m.%Y__%H:%M:%S")}.png")
-  puts "Error clicking search"
+
+rescue => exception
+  puts exception.backtrace
+  puts "Died on #{url}"
+  driver.save_screenshot("#{screenfile}")
+  FileUtils.cp("#{screenfile}", "mail")
   driver.quit
-  exit 5
+  headless.destroy
+  exit 5  
 end
 
 # Click the first details button
 begin
-  wait.until { driver.find_element(:xpath, "//a[@class='btn btn-primary btn-heavy btn-block cta-arrow-right src-srpResult-ctaButton']").displayed? }
+  url = driver.current_url
+  driver.manage.timeouts.implicit_wait = 300 
   Detailsbutton = driver.find_element(:xpath, "//a[@class='btn btn-primary btn-heavy btn-block cta-arrow-right src-srpResult-ctaButton']")
   Detailsbutton.click
-rescue
-  driver.save_screenshot("./#{Time.now.strftime("Failure__%d.%m.%Y__%H:%M:%S")}.png")
-  puts "Error clicking details"	
+
+rescue => exception
+  puts exception.backtrace
+  puts "Died on #{url}" 
+  driver.save_screenshot("#{screenfile}")
+  FileUtils.cp("#{screenfile}", "mail")
   driver.quit
-  exit 5
+  headless.destroy
+  exit 5    
 end
 
 #Click Book now
 begin
-  wait.until { driver.find_element(:xpath, "//a[@class='priceTicket-bookButton btn btn-primary cta-arrow-right btn-heavy']").displayed? }
+  url = driver.current_url
+  driver.manage.timeouts.implicit_wait = 300 
   Bookbutton = driver.find_element(:xpath, "//a[@class='priceTicket-bookButton btn btn-primary cta-arrow-right btn-heavy']")
   Bookbutton.click
-rescue
-  driver.save_screenshot("./#{Time.now.strftime("Failure__%d.%m.%Y__%H:%M:%S")}.png")	
-  puts "Error clicking book now"
+
+rescue => exception
+  puts exception.backtrace
+  puts "Died on #{url}"
+  driver.save_screenshot("#{screenfile}")
+  FileUtils.cp("#{screenfile}", "mail")
   driver.quit
-  exit 5
+  headless.destroy
+  exit 5        
 end
 
 # Continue to signing forms
 begin
-  wait.until { driver.find_element(:xpath, "//a[@class='btn btn-primary cta-arrow-right pull-right']").displayed? }	
+  url = driver.current_url
+  driver.manage.timeouts.implicit_wait = 300 
   Continuebutton = driver.find_element(:xpath, "//a[@class='btn btn-primary cta-arrow-right pull-right']")
   Continuebutton.location_once_scrolled_into_view
   Continuebutton.click
-  driver.save_screenshot("./#{Time.now.strftime("Success__%d.%m.%Y__%H:%M:%S")}.png")
-rescue
-  driver.save_screenshot("./#{Time.now.strftime("Failure__%d.%m.%Y__%H:%M:%S")}.png")
-  puts "Error proceeding to forms"	
+
+rescue => exception
+  puts exception.backtrace
+  puts "Died on #{url}"
+  driver.save_screenshot("#{screenfile}")
+  FileUtils.cp("#{screenfile}", "mail")
   driver.quit
-  exit 5
+  headless.destroy
+  exit 5  
 end
+
+# Insert postcode and look for the location to appear
+begin
+  url = driver.current_url
+  driver.manage.timeouts.implicit_wait = 300 
+  Postcode = driver.find_element(:name, "addressFormBean.postCode")
+  Postcode.location_once_scrolled_into_view
+  Postcode.send_keys "GU249DQ"
+  main_window = driver.window_handle
+  driver.manage.timeouts.implicit_wait = 300 
+  Findbutton = driver.find_element(:xpath, "//a[@class='btn btn-default']")
+  Findbutton.location_once_scrolled_into_view
+  Findbutton.click
+  sleep 5
+  driver.switch_to.window(main_window)
+
+  driver.manage.timeouts.implicit_wait = 300
+  Streetname = driver.find_element(:name, "addressFormBean.streetAddress")
+  Streetname.location_once_scrolled_into_view
+  unless Streetname.attribute('value').include? "PILGRIMS WAY"
+    fail "Street box does not contain PILGRIMS WAY"
+  end
+
+  driver.manage.timeouts.implicit_wait = 300
+  City = driver.find_element(:name, "addressFormBean.city")
+  City.location_once_scrolled_into_view
+  unless City.attribute('value').include? "WOKING"
+    fail "City box does not contain WOKING"
+  end
+
+  driver.manage.timeouts.implicit_wait = 300
+  CountryDropdown = driver.find_element(:name, "addressFormBean.country")
+  CountryDropdown.location_once_scrolled_into_view
+  Option = CountryDropdown.find_element(tag_name: "option")
+  unless Option.attribute('value').include? "UK"
+    fail "Country box does not contain United Kingdom"
+  end
+  driver.save_screenshot("#{screenfile}")
+
+rescue => exception
+  puts exception.backtrace
+  puts "Died on #{url}"
+  driver.save_screenshot("#{screenfile}")
+  FileUtils.cp("#{screenfile}", "mail")
+  driver.quit
+  headless.destroy
+  exit 5  
+end
+
+
 
 puts "UK is well!"
 
 driver.close
 driver.quit
+headless.destroy
 exit retval
