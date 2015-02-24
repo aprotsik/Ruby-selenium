1863eb69afa38ae9b59adde004b36bb1cdaa56bc
root <root@epbyminw1764t1.epam.com>
2015-02-24 16:24:49 +0300

    New initial commit

--- env.sh
+++
@@ -1,8 +0,0 @@
-export PATH="/usr/local/rvm/gems/ruby-1.9.3-p551/bin:/usr/local/rvm/gems/ruby-1.9.3-p551@global/bin:/usr/local/rvm/rubies/ruby-1.9.3-p551/bin:$PATH"
-export GEM_HOME='/usr/local/rvm/gems/ruby-1.9.3-p551'
-export GEM_PATH='/usr/local/rvm/gems/ruby-1.9.3-p551:/usr/local/rvm/gems/ruby-1.9.3-p551@global'
-export MY_RUBY_HOME='/usr/local/rvm/rubies/ruby-1.9.3-p551'
-export IRBRC='/usr/local/rvm/rubies/ruby-1.9.3-p551/.irbrc'
-unset MAGLEV_HOME
-unset RBXOPT
-export RUBY_VERSION='ruby-1.9.3-p551'
