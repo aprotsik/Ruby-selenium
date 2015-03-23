c956ed22f4c27a0f366c2814df28d8299a6ed180
aprotsik <anzhei.protsik@gmail.com>
2015-03-23 12:00:46 +0300

    Update vrijuit_nl.rb

--- scripts/vrijuit_nl.rb
+++ scripts/vrijuit_nl.rb
@@ -14,8 +14,8 @@ FileUtils.mkdir_p 'vrijuit.nl'
 FileUtils.rm_rf(Dir.glob('vrijuit.nl/*'))
 
 driver.manage.window.maximize
-driver.manage.timeouts.page_load = 60
-driver.manage.timeouts.implicit_wait = 60 
+driver.manage.timeouts.page_load = 30
+driver.manage.timeouts.implicit_wait = 30 
 
 def teardown(driver,screenfile,retval)
   driver.save_screenshot("vrijuit.nl/#{screenfile}")
