a32eab296ff31f00311bb100cae3e674b64ab8df
aprotsik <anzhei.protsik@gmail.com>
2015-03-23 12:44:14 +0300

    Update thomascook_be.rb

--- scripts/thomascook_be.rb
+++ scripts/thomascook_be.rb
@@ -14,8 +14,8 @@ FileUtils.mkdir_p 'thomascook.be'
 FileUtils.rm_rf(Dir.glob('thomascook.be/*'))
 
 driver.manage.window.maximize
-driver.manage.timeouts.page_load = 60
-driver.manage.timeouts.implicit_wait = 60 
+driver.manage.timeouts.page_load = 30
+driver.manage.timeouts.implicit_wait = 30 
 
 def teardown(driver,screenfile,retval)
   driver.save_screenshot("thomascook.be/#{screenfile}")
