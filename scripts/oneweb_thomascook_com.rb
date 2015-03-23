98296efeb3d889be571d8390a8123d2336417f1e
aprotsik <anzhei.protsik@gmail.com>
2015-03-23 12:43:54 +0300

    Update oneweb_thomascook_com.rb

--- scripts/oneweb_thomascook_com.rb
+++ scripts/oneweb_thomascook_com.rb
@@ -14,8 +14,8 @@ FileUtils.mkdir_p 'oneweb.thomascook.com'
 FileUtils.rm_rf(Dir.glob('oneweb.thomascook.com/*'))
 
 driver.manage.window.maximize
-driver.manage.timeouts.page_load = 60
-driver.manage.timeouts.implicit_wait = 60 
+driver.manage.timeouts.page_load = 30
+driver.manage.timeouts.implicit_wait = 30 
 
 def teardown(driver,screenfile,retval)
   driver.save_screenshot("oneweb.thomascook.com/#{screenfile}")
