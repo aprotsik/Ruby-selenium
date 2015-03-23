d013067ab4a479fb1f4b45d82346d1997e084b7c
aprotsik <anzhei.protsik@gmail.com>
2015-03-23 12:41:52 +0300

    Update airtours_co_uk.rb

--- scripts/airtours_co_uk.rb
+++ scripts/airtours_co_uk.rb
@@ -14,8 +14,8 @@ FileUtils.mkdir_p 'airtours.co.uk'
 FileUtils.rm_rf(Dir.glob('airtours.co.uk/*'))
 
 driver.manage.window.maximize
-driver.manage.timeouts.page_load = 60
-driver.manage.timeouts.implicit_wait = 60 
+driver.manage.timeouts.page_load = 30
+driver.manage.timeouts.implicit_wait = 30 
 
 def teardown(driver,screenfile,retval)
   driver.save_screenshot("airtours.co.uk/#{screenfile}")
