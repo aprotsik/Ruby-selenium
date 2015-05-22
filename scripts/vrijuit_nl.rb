bfcdcea89cb38723419579f72c9723c22e6cd9bc
unknown <Anzhei_Protsik@EPBYMINW1764.minsk.epam.com>
2015-05-22 13:34:26 +0300

    Committer: unknown <Anzhei_Protsik@EPBYMINW1764.minsk.epam.com>  On branch half-manual 	modified:   vrijuit_nl.rb

--- scripts/vrijuit_nl.rb
+++ scripts/vrijuit_nl.rb
@@ -153,7 +153,7 @@ end
 else
 
 begin
-  driver.find_element(:id, "calcbuttonspan_extra").click
+  driver.find_element(:id, "calcbuttonspan_calctransport").click
   driver.find_element(:xpath, "//span[@class='label bookNowButton']").click
 rescue => exception
   ret_ind = true
